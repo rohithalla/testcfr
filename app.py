4fca247a047fda1c652a7f6e6c8e7facb7952a7c
ROHITH ALLA <rohit.alla2000@gmail.com>
2021-12-05 19:11:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def home():
     return render_template('login.html',nl=nl,pl=pl,kl=kl,templ=templ,huml=huml,moistl=moistl,phl=phl)
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run(debug=False)
